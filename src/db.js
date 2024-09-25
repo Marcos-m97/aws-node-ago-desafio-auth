5a02b905b6cf99ccb76fb94c00f7340f0e359f8b
Marcos Martins <martinsmarcos900@gmail.com>
2024-09-25 13:08:35 -0300

    feat: add database connection in db.js

---
+++ src/db.js
@@ -0,0 +1,22 @@
+const mysql2 = require('mysql2');
+
+
+const connection = mysql2.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
+
+
+
+// Conectar ao banco de dados
+connection.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conexão com o banco de dados estabelecida com sucesso!');
+});
+
+module.exports = connection;
--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-
 const port = 3000
 
-const conn = require('./db/connDB')
 
 // Middleware para parsing de JSON
 app.use(express.json())
-
 
 
 // Inicia o servidor
