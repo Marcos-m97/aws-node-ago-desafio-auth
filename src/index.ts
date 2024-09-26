5c2a49d3fb914a0334a95c5e36a0f3f2cea82bd0
Marcos Martins <martinsmarcos900@gmail.com>
2024-09-26 01:29:01 -0300

    feat: configure UUID generation for user IDs

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,13 @@ import 'dotenv/config'
 
 // importanto o express
 import express from 'express'
+import { v4 as uuidv4 } from 'uuid' 
 
 //instanciando o express
 const app = express()
 
-// imprtando a conexao com o db enquanto nao enho as rotas 
+// imprtando a conexao com o db enquanto nao enho as rotas
 import connection from './db.js'
-
 
 // definindo a porta
 const port = 3000
@@ -17,20 +17,41 @@ const port = 3000
 // midllewere body-parser
 app.use(express.json())
 
-app.get("/", function (req, res) {
-  const qry = 'SELECT * FROM users'
-  connection.query(qry, function (err, data) {
+
+app.post('/api/v1/users/add', function (req, res) {
+  const id = uuidv4() // Gera o UUID
+  const { name, email, password } = req.body
+
+  if (!name || !email || !password) {
+    res.status(400).send('error: name is required')
+  }
+
+  const qry =
+    'INSERT INTO users (id, name, email, password) VALUES (?, ?, ?, ?)'
+
+  connection.query(qry, [id, name, email, password], function (err, data) {
     if (err) {
-      res.status(500).send("ops")
+      res.status(500).send('ops')
     }
-  console.log(data)
-  res.send(data)
+    console.log(id)
+    res.status(201).send('cadastrado')
   })
 })
 
-//conexao com o servidor 
-    app.listen(port, () => { 
-      console.log(`Servidor rodando na porta ${port}`)
-    })
-  
 
+// rota teste
+app.get('/', function (req, res) {
+  const qry = 'SELECT * FROM users'
+  connection.query(qry, function (err, data) {
+    if (err) {
+      res.status(500).send('ops')
+    }
+    console.log(data)
+    res.send(data)
+  })
+})
+
+//conexao com o servidor
+app.listen(port, () => {
+  console.log(`Servidor rodando na porta ${port}`)
+})
