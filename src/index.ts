8dea6b07a814fe50bda48f39d249c8c1315ede66
Marcos Martins <martinsmarcos900@gmail.com>
2024-09-25 23:21:39 -0300

    feat: add initial TypeScript configuration
    - Set up tsconfig.json
    - Added TypeScript as a development dependency
    - Updated package.json to use TypeScript

--- src/index.ts
+++ src/index.ts
@@ -10,11 +10,23 @@ const app = express()
 // imprtando a conexao com o db enquanto nao enho as rotas 
 import connection from './db.js'
 
+
 // definindo a porta
 const port = 3000
 
 // midllewere body-parser
 app.use(express.json())
+
+app.get("/", function (req, res) {
+  const qry = 'SELECT * FROM users'
+  connection.query(qry, function (err, data) {
+    if (err) {
+      res.status(500).send("ops")
+    }
+  console.log(data)
+  res.send(data)
+  })
+})
 
 //conexao com o servidor 
     app.listen(port, () => { 
