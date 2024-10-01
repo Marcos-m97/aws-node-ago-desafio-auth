f42a4b667229e01c058e8bfca1979cb94ab0d239
Marcos Martins <martinsmarcos900@gmail.com>
2024-09-30 23:34:24 -0300

    feat: Added custom error class `AppErrors` to allow for structured error responses with specific status codes and messages.

---
+++ src/uteis/errors.ts
@@ -0,0 +1,11 @@
+class AppErrors {
+ public message: string
+ public code: number
+
+  constructor(message: string, code: number) {
+    this.message = message,
+    this.code = code
+  }
+}
+
+export default AppErrors
